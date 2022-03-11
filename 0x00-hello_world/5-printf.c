564b80478bf072e32e9f681ef3cd49cf134244fb
Wesley Kibagendi <wesoon98@gmail.com>
2022-03-10 18:51:06 -0800

    Prints to stdout by printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - prints string to stdout
+* Description: prints string using prntf function in stdio
+* Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
